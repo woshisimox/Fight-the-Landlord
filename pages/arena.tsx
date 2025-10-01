9ab821edc211d7bf8eafdf7976e7ff68697e095a
woshisimox <45958997+woshisimox@users.noreply.github.com>
2025-10-01 19:40:20 +0800

    rr

--- pages/arena.tsx
+++
@@ -1,2 +0,0 @@
-import Home from './index';
-export default function ArenaPage(){ return <Home forceView="arena" />; }
--- pages/settings.tsx
+++
@@ -1,2 +0,0 @@
-import Home from './index';
-export default function SettingsPage(){ return <Home forceView="settings" />; }
