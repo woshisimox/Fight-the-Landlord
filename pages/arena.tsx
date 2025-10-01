d0dccd86978865152261055e8b3f881848280c8e
woshisimox <45958997+woshisimox@users.noreply.github.com>
2025-10-01 09:28:38 +0800

    10.2

--- pages/arena.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-export default function ArenaRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/?view=arena'); }, [router]);
-  return null;
-}
--- pages/settings.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-export default function SettingsRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/?view=settings'); }, [router]);
-  return null;
-}
