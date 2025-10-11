3a2d78a66e5128ff2799e0c6dda7ed1bdd57943d
woshisimox <45958997+woshisimox@users.noreply.github.com>
2025-10-11 18:38:05 +0800

    dddd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 
-<div style={{ marginTop: "20px" }}>
-  <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 6 }}>
-    <button>开始</button>
-    <button>停止</button>
-    <span>剩余局数: 10</span>
-  </div>
-</div>
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 6 }}>
+        <button>开始</button>
+        <button>停止</button>
+        <span>剩余局数: 10</span>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
