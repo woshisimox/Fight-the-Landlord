8cf7c68129d75943d5d6fd885649336a18e00cbe
woshisimox <45958997+woshisimox@users.noreply.github.com>
2025-10-12 22:07:38 +0800

    34

--- pages/index.tsx
+++ pages/index.tsx
@@ -2352,7 +2352,7 @@ const [lang, setLang] = useState<Lang>(() => {
     {/* 右列：表单行网格（右侧统一成“标签-控件”两列） */}
     <div
       style={{
-        gridColumn:'5 / 6',
+        gridColumn:'3 / 4',
         display:'grid',
         gridTemplateColumns:'96px 1fr',
         alignItems:'center',
