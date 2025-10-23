238fbb1b25821a9e944d133ef5626e91fd0f205c
woshisimox <45958997+woshisimox@users.noreply.github.com>
2025-10-23 22:49:11 +0800

    Delete pages directory

--- pages/knockout.tsx
+++
@@ -1,9 +0,0 @@
-import { createHome } from './index';
-
-const KnockoutHome = createHome({
-  defaults: { knockout: true },
-  knockoutLocked: true,
-  variant: 'knockout',
-});
-
-export default KnockoutHome;
